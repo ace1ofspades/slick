8b444307c1780379ec8f234e95db9a59268b5d8f
Yusuf Tekin <tekinjoseph@gmail.com>
2024-01-30 05:51:06 +0200

    Update slick.js

--- slick/slick.js
+++ slick/slick.js
@@ -2075,7 +2075,7 @@
 
         if (_.options.vertical === false && _.options.variableWidth === false) {
             _.slideWidth = Math.ceil(_.listWidth / _.options.slidesToShow);
-            _.$slideTrack.width(Math.ceil((_.slideWidth * _.$slideTrack.children('.slick-slide').length)));
+            _.$slideTrack.width(Math.ceil((_.$slideTrack.children('.slick-slide').length < _.options.slidesToShow?_.listWidth:_.slideWidth * _.$slideTrack.children('.slick-slide').length)));
 
         } else if (_.options.variableWidth === true) {
             _.$slideTrack.width(5000 * _.slideCount);
